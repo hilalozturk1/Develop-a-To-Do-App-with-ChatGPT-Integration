3d5283a7b37a709e363f8f498f05215f93193884
hilalozturk1 <ozhilal222@gmail.com>
2025-03-20 16:04:13 +0300

    login form

---
+++ frontend/src/components/Form.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Form, Input, message } from "antd";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const LoginForm: React.FC = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const navigate = useNavigate();
+
+  const onFinish = async (values: any) => {
+    console.log("onfinish");
+    try {
+      const response = await axios.post(
+        `http://localhost:5000/api/auth/${isLogin ? "login" : "register"}`,
+        values
+      );
+      if (isLogin) {
+        localStorage.setItem("token", response.data.token);
+        navigate("/todos");
+      } else {
+        message.success("User registered successfully");
+        setIsLogin(true);
+      }
+    } catch (error: any) {
+      message.error(error.response?.data.message || "An error occurred");
+    }
+  };
+
+  return (
+    <Form
+      name="login"
+      initialValues={{ remember: true }}
+      style={{ maxWidth: 360 }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name="username"
+        rules={[{ required: true, message: "Please input your Username!" }]}
+      >
+        <Input prefix={<UserOutlined />} placeholder="Username" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: "Please input your Password!" }]}
+      >
+        <Input
+          prefix={<LockOutlined />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+
+      <Form.Item>
+        <Button block type="primary" htmlType="submit">
+          {isLogin ? "Log in" : "Register"}
+        </Button>
+        or{" "}
+        <a href="" onClick={() => setIsLogin(!isLogin)}>
+          Register now!
+        </a>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default LoginForm;
