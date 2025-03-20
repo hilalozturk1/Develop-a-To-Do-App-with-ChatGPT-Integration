c9fa347f9fb4e9cadc2989253426fd79188cb6a4
hilalozturk1 <ozhilal222@gmail.com>
2025-03-20 16:21:37 +0300

    authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,34 @@
 import "./App.css";
 
+import React, { JSX } from "react";
+
 import Login from "./pages/Login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Todos from "./pages/Todos";
+
+const isAuthenticated = () => {
+  return !!localStorage.getItem("token");
+};
+
+const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+  return isAuthenticated() ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/todos" element={<Todos />} />
+          <Route
+            path="/todos"
+            element={
+              <PrivateRoute>
+                <Todos />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/" element={<Navigate to="/todos" />} />
         </Routes>
       </BrowserRouter>
     </div>
