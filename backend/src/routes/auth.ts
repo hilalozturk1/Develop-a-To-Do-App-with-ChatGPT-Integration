815ec06fce5086baa39bd5f67540c2b5b4ac3d5a
hilalozturk1 <ozhilal222@gmail.com>
2025-03-20 21:04:17 +0300

    login middleware

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,5 +1,6 @@
 import { validateUser, UserModel } from "../models/User";
 
+import jwt from "jsonwebtoken";
 import express from "express";
 import dotenv from "dotenv";
 
@@ -27,4 +28,30 @@ router.post("/register", async (req: any, res: any) => {
     res.status(500).json({ message: err.message });
   }
 });
+
+router.post("/login", async (req: any, res: any) => {
+  try {
+    const { username, password } = req.body;
+    const userData = validateUser({ username, password });
+
+    if (userData.error) {
+      return res.status(400).json({ message: "Validation error" });
+    }
+
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ message: "it could not find this user." });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, {
+      expiresIn: "1h",
+    });
+
+    res.json({ token });
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 export default router;
