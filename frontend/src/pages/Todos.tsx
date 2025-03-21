7f2a72db3c147e3bb614b4480827d65b183ad0b3
hilalozturk1 <ozhilal222@gmail.com>
2025-03-22 02:11:58 +0300

    logout

--- frontend/src/pages/Todos.tsx
+++ frontend/src/pages/Todos.tsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 import { SearchBar } from "../components/SearchBar";
 import { TodoList } from "../components/TodoList";
 
-import { Row, Col, Card, message } from "antd";
+import { Row, Col, Card, message, Button } from "antd";
 import { Header } from "antd/es/layout/layout";
 
 function Todos() {
   const token = localStorage.getItem("token");
+  const navigate = useNavigate();
 
   const [loading, setLoading] = useState<boolean>(false);
   const [isCreated, setIsCreated] = useState<boolean>(false);
@@ -33,47 +35,63 @@ function Todos() {
       setLoading(false);
     }
   };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+    message.success("Logged out successfully!");
+  };
   return (
-    <Row
-      justify="center"
-      align="middle"
-      gutter={[0, 20]}
-      className="todos-container"
-    >
-      <Col
-        xs={{ span: 23 }}
-        sm={{ span: 23 }}
-        md={{ span: 21 }}
-        lg={{ span: 20 }}
-        xl={{ span: 18 }}
+    <>
+      <Button
+        type="primary"
+        danger
+        style={{ float: "right", marginTop: "20px", marginRight: "20px" }}
+        onClick={handleLogout}
       >
-        <Header title="Add Todo" />
-      </Col>
+        Log Out
+      </Button>
+      <Row
+        justify="center"
+        align="middle"
+        gutter={[0, 20]}
+        className="todos-container"
+      >
+        <Col
+          xs={{ span: 23 }}
+          sm={{ span: 23 }}
+          md={{ span: 21 }}
+          lg={{ span: 20 }}
+          xl={{ span: 18 }}
+        >
+          <Header title="Add Todo" />
+        </Col>
 
-      <Col
-        xs={{ span: 23 }}
-        sm={{ span: 23 }}
-        md={{ span: 21 }}
-        lg={{ span: 20 }}
-        xl={{ span: 18 }}
-      >
-        <Card title="Create a new todo">
-          <SearchBar onFormSubmit={handleFormSubmit} />
-        </Card>
-      </Col>
+        <Col
+          xs={{ span: 23 }}
+          sm={{ span: 23 }}
+          md={{ span: 21 }}
+          lg={{ span: 20 }}
+          xl={{ span: 18 }}
+        >
+          <Card title="Create a new todo">
+            <SearchBar onFormSubmit={handleFormSubmit} />
+          </Card>
+        </Col>
 
-      <Col
-        xs={{ span: 23 }}
-        sm={{ span: 23 }}
-        md={{ span: 21 }}
-        lg={{ span: 20 }}
-        xl={{ span: 18 }}
-      >
-        <Card title="Todo List">
-          <TodoList isCreated={isCreated} />
-        </Card>
-      </Col>
-    </Row>
+        <Col
+          xs={{ span: 23 }}
+          sm={{ span: 23 }}
+          md={{ span: 21 }}
+          lg={{ span: 20 }}
+          xl={{ span: 18 }}
+        >
+          <Card title="Todo List">
+            <TodoList isCreated={isCreated} />
+          </Card>
+        </Col>
+      </Row>
+    </>
   );
 }
 
