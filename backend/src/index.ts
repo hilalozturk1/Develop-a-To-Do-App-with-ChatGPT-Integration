b90864d608bca9fd0e96636d2bfb2990aa36fd5b
hilalozturk1 <ozhilal222@gmail.com>
2025-03-21 17:39:47 +0300

    static files upload

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import router from "./routes/auth";
+import routerTodo from "./routes/todo";
+import path from "path";
 
 const app = express();
+app.use("/uploads", express.static(path.join(__dirname, "../public/uploads")));
 
 app.use(cors());
 
@@ -10,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", router);
+app.use("/api/todos", routerTodo.routerTodo);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to the Node.js + TypeScript API!");
