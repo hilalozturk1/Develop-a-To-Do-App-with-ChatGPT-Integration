19ac668ad8d821071052693629edd2c0df7c3ddd
hilalozturk1 <ozhilal222@gmail.com>
2025-03-21 18:04:29 +0300

    added image and file fields

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,22 +1,41 @@
-import React from 'react';
-import { Form, Row, Col, Button, Input } from 'antd';
-import { PlusCircleFilled } from '@ant-design/icons';
+import React, { useState } from "react";
 
-import { ITodo } from '../store/todo/models/todo.models';
-
+import { Form, Row, Col, Button, Input, Upload } from "antd";
+import { PlusCircleFilled, UploadOutlined } from "@ant-design/icons";
 interface IAddTodoFormProps {
-  onFormSubmit: (todo: ITodo) => void;
+  onFormSubmit: (formData: FormData) => void;
 }
 
 export const SearchBar: React.FC<IAddTodoFormProps> = ({ onFormSubmit }) => {
   const [form] = Form.useForm();
+  const [fileList, setFileList] = useState<any[]>([]);
+  const [imageList, setImageList] = useState<any[]>([]);
 
   const onFinish = () => {
-    onFormSubmit({
-      name: form.getFieldValue('name'),
-    });
+    const formData = new FormData();
+
+    formData.append("name", form.getFieldValue("name"));
+
+    if (imageList.length > 0) {
+      formData.append("image", imageList[0].originFileObj);
+    }
+
+    if (fileList.length > 0) {
+      formData.append("file", fileList[0].originFileObj);
+    }
+
+    onFormSubmit(formData);
+    setFileList([]);
+    setImageList([]);
 
     form.resetFields();
+  };
+  const handleFileChange = ({ fileList }: any) => {
+    setFileList(fileList);
+  };
+
+  const handleImageChange = ({ fileList }: any) => {
+    setImageList(fileList);
   };
 
   return (
@@ -24,16 +43,39 @@ export const SearchBar: React.FC<IAddTodoFormProps> = ({ onFormSubmit }) => {
       form={form}
       onFinish={onFinish}
       layout="horizontal"
-      className="todo-form"
+      className="todo-form w-100"
     >
       <Row gutter={20}>
         <Col xs={24} sm={24} md={17} lg={19} xl={20}>
           <Form.Item
-            name={'name'}
-            rules={[{ required: true, message: 'This field is required' }]}
+            name={"name"}
+            rules={[{ required: true, message: "This field is required" }]}
           >
             <Input placeholder="What needs to be done?" />
           </Form.Item>
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={10} xl={8}>
+          <Upload
+            accept="image/*"
+            maxCount={1}
+            fileList={imageList}
+            onChange={handleImageChange}
+            beforeUpload={() => false}
+          >
+            <Button icon={<UploadOutlined />}>Upload Image</Button>
+          </Upload>
+        </Col>
+
+        <Col xs={24} sm={24} md={12} lg={10} xl={8}>
+          <Upload
+            accept=".pdf,.doc,.docx,.txt,.png,.jpg,.jpeg"
+            maxCount={1}
+            fileList={fileList}
+            onChange={handleFileChange}
+            beforeUpload={() => false}
+          >
+            <Button icon={<UploadOutlined />}>Upload File</Button>
+          </Upload>
         </Col>
         <Col xs={24} sm={24} md={7} lg={5} xl={4}>
           <Button type="primary" htmlType="submit" block>
