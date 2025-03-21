a8728deb1f12da247445139ee93d90f1732ea7fa
hilalozturk1 <ozhilal222@gmail.com>
2025-03-21 17:58:41 +0300

    handle fetch todos and form submit

--- frontend/src/pages/Todos.tsx
+++ frontend/src/pages/Todos.tsx
@@ -1,26 +1,61 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import { SearchBar } from "../components/SearchBar";
 import { TodoList } from "../components/TodoList";
+
 import { Row, Col, Card, message } from "antd";
-import { ITodo } from "../store/todo/models/todo.models";
 import { Header } from "antd/es/layout/layout";
 
+import { ITodo } from "../store/todo/models/todo.models";
+
 function Todos() {
-  const data: ITodo[] = [
-    {
-      name: 'Ant Design Title 1',
-    },
-    {
-      name: 'Ant Design Title 2',
-    },
-    {
-      name: 'Ant Design Title 3',
-    },
-    {
-      name: 'Ant Design Title 4',
-    },
-  ];
-  const handleFormSubmit = (todo: ITodo): void => {
-    message.success("Todo added!");
+  const token = localStorage.getItem("token");
+
+  const [data, setData] = useState<ITodo[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const fetchTodos = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("http://localhost:5000/api/todos", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setData(response.data.todos);
+      setLoading(false);
+    } catch (error: any) {
+      message.error("Failed to fetch todos!");
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const handleFormSubmit = async (formData: FormData): Promise<void> => {
+
+    try {
+      setLoading(true);
+      const response = await axios.post(
+        "http://localhost:5000/api/todos",
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      message.success("Todo added successfully!");
+      fetchTodos();
+    } catch (error: any) {
+      message.error("Failed to add todo!");
+    } finally {
+      setLoading(false);
+    }
   };
   return (
     <Row
@@ -36,9 +71,7 @@ function Todos() {
         lg={{ span: 20 }}
         xl={{ span: 18 }}
       >
-        <Header
-          title="Add Todo"
-        />
+        <Header title="Add Todo" />
       </Col>
 
       <Col
@@ -61,9 +94,7 @@ function Todos() {
         xl={{ span: 18 }}
       >
         <Card title="Todo List">
-          <TodoList
-            todos={data}
-          />
+          <TodoList todos={data} />
         </Card>
       </Col>
     </Row>
