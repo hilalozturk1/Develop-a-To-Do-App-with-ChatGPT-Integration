2d8a6e5ecbfc6e5a41be9cfd55346284b49339cb
hilalozturk1 <ozhilal222@gmail.com>
2025-03-27 20:36:00 +0300

    edited upload middleware

--- backend/src/middlewares/fileUpload.ts
+++ backend/src/middlewares/fileUpload.ts
@@ -2,11 +2,14 @@ import multer from 'multer';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, './public/uploads/');
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
   }
 });
 
-export const uploadMiddleware = multer({ storage });
+export const uploadMiddleware = multer({ storage }).fields([
+  { name: 'image', maxCount: 1 },
+  { name: 'file', maxCount: 1 } 
+]);
