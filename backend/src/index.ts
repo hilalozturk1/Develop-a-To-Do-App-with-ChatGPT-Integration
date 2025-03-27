872329e47349e9a2cd362ba8371532d43c3d3d57
hilalozturk1 <ozhilal222@gmail.com>
2025-03-27 20:39:41 +0300

    fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,25 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
-import router from "./routes/auth";
-import routerTodo from "./routes/todo";
 import path from "path";
+import dotenv from "dotenv";
+import todoRoutes from "./routes/todoRoutes";
 import { errorHandler } from "./middlewares/errorHandler";
+import router from "./routes/auth";
+
+dotenv.config();
 
 const app = express();
-app.use("/uploads", express.static(path.join(__dirname, "../public/uploads")));
 
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(errorHandler);
-app.use("/api/auth", router);
-app.use("/api/todos", routerTodo.routerTodo);
+app.use("/uploads", express.static(path.join(__dirname, "../public/uploads")));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to the Node.js + TypeScript API!");
-});
+app.use("/api/auth", router);
+app.use("/api/todos", todoRoutes);
+
+app.use(errorHandler);
 
 export default app;
