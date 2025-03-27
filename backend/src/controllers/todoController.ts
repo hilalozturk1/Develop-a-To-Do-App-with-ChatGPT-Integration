d5058009b4839314a7c1bb933cd8a2ea27631aef
hilalozturk1 <ozhilal222@gmail.com>
2025-03-27 16:55:15 +0300

    added all todos as controller

--- backend/src/controllers/todoController.ts
+++ backend/src/controllers/todoController.ts
@@ -1,15 +1,102 @@
-import { Request, Response, NextFunction } from 'express';
-import { TodoService } from '../services/todoService';
+import { NextFunction, RequestHandler } from "express";
+import { TodoService } from "../services/todoService";
 
 export class TodoController {
   constructor(private todoService: TodoService) {}
 
-  async createTodo(req: Request, res: Response, next: NextFunction) {
+  createTodo: RequestHandler = async (
+    req: any,
+    res: any,
+    next: NextFunction
+  ) => {
     try {
-      const todo = await this.todoService.createTodo(req.body);
+      const todoData = {
+        ...req.body,
+        userId: req.user._id,
+        imageUrl: req.file ? `/uploads/${req.file.filename}` : undefined,
+        fileUrl: req.file ? `/uploads/${req.file.filename}` : undefined,
+      };
+
+      const todo = await this.todoService.createTodo(todoData);
       return res.status(201).json(todo);
     } catch (error) {
       next(error);
     }
-  }
+  };
+
+  getAllTodos: RequestHandler = async (req: any, res: any, next) => {
+    try {
+      const todos = await this.todoService.getAllTodos(req.user._id);
+      return res.status(200).json(todos);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  getTodoById: RequestHandler = async (req: any, res: any, next) => {
+    try {
+      const todo = await this.todoService.getTodoById(
+        req.params.id,
+        req.user._id
+      );
+      return res.status(200).json(todo);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  updateTodo: RequestHandler = async (req: any, res: any, next) => {
+    try {
+      const updateData = {
+        ...req.body,
+        imageUrl: req.file ? `/uploads/${req.file.filename}` : undefined,
+        fileUrl: req.file ? `/uploads/${req.file.filename}` : undefined,
+      };
+
+      const todo = await this.todoService.updateTodo(
+        req.params.id,
+        req.user._id,
+        updateData
+      );
+      return res.status(200).json(todo);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  deleteTodo: RequestHandler = async (req: any, res: any, next) => {
+    try {
+      const result = await this.todoService.deleteTodo(
+        req.params.id,
+        req.user._id
+      );
+      return res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  toggleTodoStatus: RequestHandler = async (req: any, res: any, next) => {
+    try {
+      const todo = await this.todoService.toggleTodoStatus(
+        req.params.id,
+        req.user._id
+      );
+      return res.status(200).json(todo);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  searchTodos: RequestHandler = async (req: any, res: any, next) => {
+    try {
+      const todos = await this.todoService.searchTodos(
+        req.user._id,
+        req.query.q as string
+      );
+      return res.status(200).json(todos);
+    } catch (error) {
+      next(error);
+    }
+  };
 }
